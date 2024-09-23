4b5a0b6734216baa8388a57c9a17ee38177fbc6d
Deepan7010 <deepanchakkaravarthi49@gmail.com>
2024-09-23 13:10:29 +0530

    first commit

---
+++ src/main/java/com/tech/setbox/PaypalController.java
@@ -0,0 +1,14 @@
+package com.tech.setbox;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class PaypalController {
+	
+	@Autowired
+	PaypalService service;
+	
+	
+	
+}
---
+++ src/main/java/com/tech/setbox/SpringPaypalExampleApplication.java
@@ -0,0 +1,13 @@
+package com.tech.setbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPaypalExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPaypalExampleApplication.class, args);
+	}
+
+}
